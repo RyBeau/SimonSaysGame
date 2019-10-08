3f141e1fb1df888fd4055fbb20e04cf9c5056897
Raymond Steve Jr Fernandez Tamse <rft22@cs19119bs.canterbury.ac.nz>
2019-10-08 19:58:35 +1300

    Created for use by game.c

---
+++ navinput.h
@@ -0,0 +1,35 @@
+/*
+ * Declares the functions of navinput.c
+ * Currently lacks function comments.
+ *
+ * Authors: Raymond Tamse, Ryan Beaumont
+ * Date: October 9 2019
+ */
+
+ #ifndef NAVSWITCH_H
+ #define NAVSWITCH_H
+
+/*
+ * Takes a char type variable and displays one of the four arrows
+ * or the dot corresponding to the specific character given.
+ * Result is displayed on the LED matrix using tinygl.
+ */
+void display_character (char character);
+
+/*
+ * Returns either !, ", #, $, or %, which all correspond to
+ * one of the four arrows and the dot, depending on the player's input
+ * from the navswitch. Uses a while loop.
+ */
+char player_input ();
+
+/* Takes an integer 'seq_num' to record the required amount of characters.
+ * Uses a char* to record a sequence of characters using the navswitch.
+ * Calls 'player_input()' for the characters.
+ * Returns the head of the sequence when it finishes looping
+ * for 'seq_num' times. Uses a for loop.
+ */
+char* player_sequence (int seq_num);
+
+ #endif
+
