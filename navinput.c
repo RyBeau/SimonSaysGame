68940c65bec37159b7f490dca95a158c3b8265e1
Raymond Steve Jr Fernandez Tamse <rft22@cs18220kq.canterbury.ac.nz>
2019-10-14 20:44:15 +1300

    Now only processes player input through a pointer

--- navinput.c
+++ navinput.c
@@ -1,17 +1,14 @@
 /*
- * Implements navswitch and stores the player's input sequence.
- * Currently contains code copies from existing lab files.
- * Makes use of game_display to display the player's input sequence.
+ * Implements navswitch and processes the player's input sequence.
  *
- * Authors: Raymond Tamse
- * Date: October 13 2019
+ * @author Raymond Tamse
+ * Date: October 14 2019
  */
 
-#include <stdio.h>
-#include <string.h>
 #include "system.h"
 #include "navswitch.h"
 #include "game_display.h"
+#include "pacer.h"
 
 
 #define SEQ_MAX 20
@@ -29,65 +26,48 @@ char host_input(void)
 }
 
 /*
- * Returns either !, ", #, $, or %, which all correspond to
- * one of the four arrows and the dot, depending on the player's input
- * from the navswitch.
- * Displays the character's input on the LED matrix using game_display.
+ * Takes a char* input that gets updated depending on the player's
+ * input. Loops n times, where n/2 is the num. of symbols - the
+ * other half is for spaces.
  */
-char player_input(void)
+void player_input(char* input, int n)
 {
-    char input_char = 'A';
-    // Arrows; clockwise starting from North
-    while (input_char == 'A') {
+    int counter = 0;
+    while (counter < n) {
         if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
-            input_char = '^';
+            input[counter] = '^';
+            input[counter + 1] = ' ';
+            counter += 2;
         }
 
         if (navswitch_push_event_p (NAVSWITCH_EAST)) {
-            input_char = '>';
+            input[counter] = '>';
+            input[counter + 1] = ' ';
+            counter += 2;
         }
 
         if (navswitch_push_event_p (NAVSWITCH_SOUTH)) {
-            input_char = '.';
+            input[counter] = '.';
+            input[counter + 1] = ' ';
+            counter += 2;
         }
 
         if (navswitch_push_event_p (NAVSWITCH_WEST)) {
-            input_char = '<';
+            input[counter] = '<';
+            input[counter + 1] = ' ';
+            counter += 2;
         }
 
         if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-            input_char = '*';    // dot
+            input[counter] = '*';
+            input[counter + 1] = ' ';
+            counter += 2;   // dot
         }
+        navswitch_update();
+        pacer_wait();
     }
-    display_text(&input_char, 2);
-    navswitch_update();
-    return input_char;
-}
-
-/* Takes an integer 'seq_num' to record the required amount of characters.
- * Uses a char to record a sequence of characters using the navswitch.
- * Calls 'player_input()' for the characters.
- * Returns the sequence string when it finishes looping
- * for 'seq_num' times. Uses a for loop.
- */
-char player_sequence(int seq_num)
-{
-    char sequence[seq_num] = {""};
-    // Store player's input
-    for (int i = 0; i < seq_num; i++) {
-        sequence[i] = player_input() + ' ';
-    }
-    sequence[seq_num] = '\0';
-    return sequence;
-}
-
-int main(void)
-{
-    system_init ();
-    navswitch_init ();
-
-    return 0;
-
-
+    display_sequence(input, n); //dont want
 
 }
+
+
