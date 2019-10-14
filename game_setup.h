cfe04cac0a8883e20635a0dd961f457038b78fba
Raymond Steve Jr Fernandez Tamse <rft22@cs19090bs.canterbury.ac.nz>
2019-10-14 14:37:35 +1300

    Changed set_player() docstring according to update

--- game_setup.h
+++ game_setup.h
@@ -1,14 +1,17 @@
 /*
  * Declares the functions of game_setup.c
- * Currently lacks function comments.
  *
- * Authors: Raymond Tamse, Ryan Beaumont
- * Date: October 9 2019
+ * Authors: Raymond Tamse
+ * Date: October 13 2019
  */
+
+#ifndef GAME_SETUP_H
+#define GAME_SETUP_H
 
 #include "system.h"
 
-/* Simply prints "Simon Says" on the LED matrix.
+/*
+ * Simply prints "Simon Says" on the LED matrix.
  */
 void title_msg();
 
@@ -18,12 +21,14 @@ void title_msg();
  * If person receives '*', trasmit '!' - person is player.
  * If person receives '!', start game - person is host.
  * Uses transmit.c for IR communication.
+ * Returns an int that tells if the player is a host.
  */
-void set_player();
+int set_player();
 
 /*
  * Takes an integer 'win' to choose between 3 different messages
  * depending on the result of the game.
- * Uses display_text to display the result message.
  */
 void game_over(int win);
+
+#endif
