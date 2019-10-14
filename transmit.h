056ba2d3c06088926f2d8943690c0c88a6732fc6
Youre Name <rbe72@uclive.ac.nz>
2019-10-14 17:50:35 +1300

    Changed receiveSequence prototype, added init prototype, and comments

--- transmit.h
+++ transmit.h
@@ -4,10 +4,23 @@
 
 #include "system.h"
 
+/**
+ * This transmits the sequece given in char* sequence.
+ * @param char* sequence, data to be sent to the other board
+ * */
 void transmitSequence(char* sequence);
 
-char* receiveSequence(int n);
+/**
+ * Receives the data sent by transmitSequence and puts it in the
+ * given char* received.
+ * @param char* received, char* for the received data to go into
+ * */
+void receiveSequence(char* received);
 
+/**
+ * Intialises other modules or variables needed for the 
+ * transmit module.
+ **/
 void transmit_init(void);
 
 #endif
