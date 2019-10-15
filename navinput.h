46c9c9d2e9cbb026e2b013de2ee006e14fe3b5e3
Youre Name <rbe72@uclive.ac.nz>
2019-10-15 18:38:05 +1300

    Removed unneeded function prototypes

--- navinput.h
+++ navinput.h
@@ -8,11 +8,6 @@
  #ifndef NAVSWITCH_H
  #define NAVSWITCH_H
 
-/* Returns '*' if a player wants to be the host
- * before the game starts.
- */
-char host_input ();
-
 /*
  * Takes a char* input that gets updated depending on the player's
  * input. Loops n times, where n/2 is the num. of symbols - the
