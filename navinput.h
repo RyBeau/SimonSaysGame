5e8db9757814a4dbc20f635af697c8f51e26a3cc
Raymond Steve Jr Fernandez Tamse <rft22@cs17096jp.canterbury.ac.nz>
2019-10-15 19:26:55 +1300

    Updated docstring

--- navinput.h
+++ navinput.h
@@ -1,17 +1,18 @@
 /*
  * Declares the functions of navinput.c
  *
- * Authors: Raymond Tamse
- * Date: October 14 2019
+ * Authors: Raymond Tamse, Ryan beaumont
+ * Date: October 15 2019
  */
 
  #ifndef NAVSWITCH_H
  #define NAVSWITCH_H
 
-/*
+/**
  * Takes a char* input that gets updated depending on the player's
  * input. Loops n times, where n/2 is the num. of symbols - the
  * other half is for spaces.
+ * @param char* input, player sequence to be updated with input
  */
 void player_input (char* input, int n);
 
