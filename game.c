db082c8d72478286428b6e82a748f219723d5a9e
Youre Name <rbe72@uclive.ac.nz>
2019-10-15 18:38:27 +1300

    Removed old testing code

--- game.c
+++ game.c
@@ -20,41 +20,6 @@ int checkSequence(char* received, char* input, int n)
     }
 }
 
-/**
-int main (void)
-{
-    system_init();
-    navswitch_init();
-    pacer_init(PACER_RATE);
-    gameDisplay_init();
-    transmit_init();
-
-    char sequence[10];
-    char received[10];
-    while (1) {
-        if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
-			display_text("S ", 2);
-            player_input(sequence, 10);
-            transmitSequence(sequence, 10);
-        }
-        if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
-			display_text("R ", 2);
-            receiveSequence(received, 10);
-            display_sequence(received, 10);
-            player_input(sequence, 10);
-            if (checkSequence(received, sequence, 10)) {
-				display_text("Matches ", 8);
-            } else {
-				display_text("Wrong ", 6);
-			}
-
-        }
-        navswitch_update();
-        pacer_wait();
-    }
-}
-*/
-
 int main (void)
 {
     system_init();
@@ -88,42 +53,3 @@ int main (void)
     }
 }
 
-/**        PATTERN MATCHING TEST - SUCCESS
-int main (void)
-{
-    system_init();
-    navswitch_init();
-    pacer_init(PACER_RATE);
-    gameDisplay_init();
-    transmit_init();
-    char test1[10];
-    char test2[10];
-    while(1) {
-        player_input(test1, 10);            // assuming that seq_size is always even?
-        player_input(test2, 10);
-        if (strncmp(test1, test2, 10) == 0) {
-            display_text(" Match! ", 8);
-        } else {
-            display_text(" Fail ", 6);
-        }
-    }
-
-}
-**/
-
-/**      RANDOM SEQUENCE GENERATOR - SUCCESS
-int main (void)
-{
-    system_init();
-    navswitch_init();
-    pacer_init(PACER_RATE);
-    gameDisplay_init();
-    transmit_init();
-
-    char test1[10];
-    while (1) {
-        generate_sequence(test1, 10);
-        display_sequence(test1, 10);
-    }
-}
-*/
