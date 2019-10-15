ae8809302fea9fd601d8aa70ed176eeb89f3f8c8
Raymond Steve Jr Fernandez Tamse <rft22@cs17096jp.canterbury.ac.nz>
2019-10-15 19:29:07 +1300

    Updated docstrings

--- game_setup.h
+++ game_setup.h
@@ -1,8 +1,8 @@
 /*
  * Declares the functions of game_setup.c
  *
- * Authors: Raymond Tamse
- * Date: October 13 2019
+ * Authors: Raymond Tamse, Ryan Beaumont
+ * Date: October 15 2019
  */
 
 #ifndef GAME_SETUP_H
@@ -10,18 +10,17 @@
 
 #include "system.h"
 
-/*
+/**
  * Simply prints "Simon Says" on the LED matrix.
  */
 void title_msg();
 
-/*
- * Expects to receive '*' or '!' through IR to determine
- * who the host and the player is.
- * If person receives '*', trasmit '!' - person is player.
- * If person receives '!', start game - person is host.
- * Uses transmit.c for IR communication.
- * Returns an int that tells if the player is a host.
+/**
+ * Handles the matchmaking between two boards.
+ * Takes an int* to determine who will start first (host).
+ * Proceeds to send and receive a signal (!) when player is found.
+ *
+ * @param int* is_host, int to be updated to determine host
  */
 void set_player(int* is_host);
 
@@ -29,6 +28,6 @@ void set_player(int* is_host);
  * Takes an integer 'win' to choose between 3 different messages
  * depending on the result of the game.
  */
-void game_over(int win);
+// void game_over(int win);
 
 #endif
