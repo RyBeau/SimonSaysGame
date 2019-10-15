24dbcf0512c9f063fa53c438aa10ea19027355c1
Raymond Steve Jr Fernandez Tamse <rft22@cs17096jp.canterbury.ac.nz>
2019-10-15 19:31:43 +1300

    Moved sequenceCheck to gameplay.c; added calls

--- game.c
+++ game.c
@@ -11,14 +11,6 @@
 
 #define PACER_RATE 500
 
-int checkSequence(char* received, char* input, int n)
-{
-    if (strncmp(received, input, n) == 0) {
-        return(1);
-    } else {
-        return(0);
-    }
-}
 
 int main (void)
 {
@@ -32,7 +24,10 @@ int main (void)
     char received[10];
     int is_turn = 0;
     while (1) {
+        title_msg();
         set_player(&is_turn);
+
+
         if (is_turn) {
             display_text("Your Turn ", 10);
             player_input(sequence, 10);
@@ -42,13 +37,9 @@ int main (void)
             receiveSequence(received, 10);
             display_sequence(received, 10);
             player_input(sequence, 10);
-            if (checkSequence(received, sequence, 10)) {
-                display_text("Matches ", 8);
-                is_turn = 1;
-            } else {
-                display_text("Wrong ", 6);
-            }
+            is_turn = checkSequence(received, sequence, 10);
         }
+
         pacer_wait();
     }
 }
