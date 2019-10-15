4e9c024bb08aa9eeaf02f52069c58c21abea5666
Raymond Steve Jr Fernandez Tamse <rft22@cs17096jp.canterbury.ac.nz>
2019-10-15 19:39:38 +1300

    Reverted changes

--- game.c
+++ game.c
@@ -12,6 +12,24 @@
 #define PACER_RATE 500
 
 
+/**
+ * Takes two pointers char* which point to the host sequence and
+ * the player sequence.
+ * Compares the two sequences, and returns 1 if the sequence matches,
+ * otherwise returns 0;
+ *
+ * @param char* received, host's sequence to be compared
+ * @param char* input, player's sequence to be compared
+ */
+int checkSequence(char* received, char* input, int n)
+{
+    if (strncmp(received, input, n) == 0) {
+        return(1);
+    } else {
+        return(0);
+    }
+}
+
 int main (void)
 {
     system_init();
@@ -27,7 +45,6 @@ int main (void)
         title_msg();
         set_player(&is_turn);
 
-
         if (is_turn) {
             display_text("Your Turn ", 10);
             player_input(sequence, 10);
@@ -37,9 +54,13 @@ int main (void)
             receiveSequence(received, 10);
             display_sequence(received, 10);
             player_input(sequence, 10);
-            is_turn = checkSequence(received, sequence, 10);
+            if (checkSequence(received, sequence, 10)) {
+                display_text("Matches ", 8);
+                is_turn = 1;
+            } else {
+                display_text("Wrong ", 6);
+            }
         }
-
         pacer_wait();
     }
 }
