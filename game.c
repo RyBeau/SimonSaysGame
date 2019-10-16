acc50ff5fb9c553cb5292cca125d3a27ba402c85
Youre Name <rbe72@uclive.ac.nz>
2019-10-16 16:47:18 +1300

    Comments added to functions

--- game.c
+++ game.c
@@ -1,3 +1,12 @@
+/**
+ * @file game.c
+ * @author Ryan Beaumont
+ * @author Raymond Tamse
+ * @date 16/10/2019
+ * game.c is the main module of the game. It contains functions related to
+ * game logic and operation.
+ * */
+
 #include "system.h"
 #include "game_display.h"
 #include "transmit.h"
@@ -33,6 +42,18 @@ int checkSequence(char* received, char* input, int n)
     }
 }
 
+/**
+ * This function executes the two senarios that arise when comparing the
+ * players inputted sequence with the sequence received from the other board.
+ * If checkSequence returns true then it displays the text 'Matches' before setting
+ * the value pointed to by turn to 1. Otherwise it displays the text "Wrong" and
+ * sets the value pointed to by game_playing to 0.
+ * @param char* received, pointer to the sequence received from the other player
+ * @param char* sequence, pointer to the sequence entered by the player
+ * @param int* game_playing, pointer to game_playing int from the game_loop function
+ * @param int* is_turn, pointer to the is_turn int from the game loop function
+ * @param int seq_add, the addition character length from round progression
+ * */
 void receiving_check(char* received, char* sequence, int* game_playing, int* is_turn,int seq_add)
 {
     if (checkSequence(received, sequence, SEQ_SIZE + seq_add)) {
@@ -45,6 +66,19 @@ void receiving_check(char* received, char* sequence, int* game_playing, int* is_
     }
 }
 
+/**
+ * This function is for the turn where the player must match the sequence 
+ * that was sent to them by the other player. If first checks to see if the 
+ * received sequence is signalling that they have beaten the other player.
+ * If not then it displays received, then calls player_input to get input from
+ * the player, and finally calls receiving_check to find out if the player successfully repeated
+ * the pattern. 
+ * @param char* received, pointer to the sequence received from the other player
+ * @param char* sequence, pointer to the sequence entered by the player
+ * @param int* game_playing, pointer to game_playing int from the game_loop function
+ * @param int* is_turn, pointer to the is_turn int from the game loop function
+ * @param int seq_add, the addition character length from round progression
+ * */
 void receivingTurn(char* received, char* sequence, int* game_playing, int* is_turn,int seq_add)
 {
     receiveSequence(received, SEQ_SIZE + seq_add);
@@ -58,6 +92,16 @@ void receivingTurn(char* received, char* sequence, int* game_playing, int* is_tu
     }
 }
 
+/**
+ * This function is for the turn where the player is setting a pattern
+ * to be sent to the other player. First it indicates to the player that its
+ * there turn to enter a pattern. It then calls player_input to get that pattern.
+ * Finally it transmits that sequence and sets the value pointed to by is_turn
+ * to 0.
+ * @param char* sequence, pointer to the sequence entered by the player
+ * @param int* is_turn, pointer to the is_turn int from the game loop function
+ * @param int seq_add, the addition character length from round progression
+ * */
 void sendingTurn(char* sequence,int* is_turn, int seq_add)
 {
     display_text("Your Turn ", 10);
@@ -66,18 +110,36 @@ void sendingTurn(char* sequence,int* is_turn, int seq_add)
     *is_turn = 0;
 }
 
+/**
+ * This function uses realloc to dynamically increase the size of the
+ * char* for the sequences to be repeated by and entered by the player
+ * @param char* sequence, pointer to the sequence entered by the player
+ * @param char* received, pointer to the sequence received from the other player
+ * @param int seq_add, the addition character length from round progression
+ * */
 void reallocCharStars(char* sequence, char* received, int seq_add)
 {
 	realloc(sequence, SEQ_SIZE + seq_add);
 	realloc(received, SEQ_SIZE + seq_add);
 }
 
+/**
+ * This function frees the allocated memory given to the char* sequence 
+ * and received.
+ * @param char* sequence, pointer to the sequence entered by the player
+ * @param char* received, pointer to the sequence received from the other player
+ * */
 void freeCharStars(char* sequence, char* received)
 {
 	free(sequence);
 	free(received);
 }
 
+/**
+ * This is the loop for the gameplay aspect. It intialises all
+ * variables that are needed only during a match of the game.
+ * @param int is_turn, indicates whether its this players turn to set a sequence and transmit
+ * */
 void game_loop(int is_turn)
 {
     int turns = 1;              // keeps track of turn switches
@@ -101,6 +163,15 @@ void game_loop(int is_turn)
     freeCharStars(sequence, received);
 }
 
+/**
+ * This is the main loop for the game, here all sub-loops are called in
+ * the correct order to allow for reqeated games without restarting the 
+ * UCFK. It first calls all the init functions of modules that will be 
+ * used throughtout the game. Next it defines is_turn which is an int that
+ * holds a 0 or 1 to indicate true or false.
+ * Finally it enters the infinite while loop where it plays the title message
+ * then enters the setup loop, and finally enters the game loop.
+ * */
 int main (void)
 {
     system_init();
