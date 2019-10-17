b8ddf8511b9674b51270a8874b3c5fc39577a8b0
Youre Name <rbe72@uclive.ac.nz>
2019-10-17 23:34:46 +1300

    Added display_int function, added comments, updated function names to be more clear

--- game_display.c
+++ game_display.c
@@ -1,14 +1,16 @@
 /**
  * @file game_display.c
  * @author Ryan Beaumont
+ * @author Raymond Tamse
  * @date 9/10/2019
  * @brief Display on LED Matrix
- * @note This module contains functions for display game info on screen
+ * @note This module contains functions for display game infomation on screen
  * */
 #include "system.h"
 #include "pacer.h"
 #include "tinygl.h"
 #include "font5x7_1_arrows.h"
+#include <stdlib.h>
 
 
 #define PACER_RATE 500 //Rate for the paced loop
@@ -19,43 +21,43 @@
 /**
  * Clears and updates the LED matrix
  * */
-static void matrixClear(void)
+static void display_matrix_clear(void)
 {
     tinygl_clear();
     tinygl_update();
 
 }
 
-/**Converts the wantedHz to a number of loops of the PACER_RATE
+/**Converts the wanted_hz to a number of loops of the PACER_RATE
  * to allow for different loop pacing.
- * @param wantedHz the desired rate for the loop to run at.
- * @return Number of loops to get the wantedHz*/
-static int makeSameHz(int wantedHz)
+ * @param wanted_hz the desired rate for the loop to run at.
+ * @return Number of loops to get the wanted_hz*/
+static int display_get_hz(int wanted_hz)
 {
-    return PACER_RATE / wantedHz;
+    return PACER_RATE / wanted_hz;
 }
 
 /**The function is the loop for dislaying the text set using tinygl_text
  * It will loop for as long as need to display n letters on the matrix.
  * Then clear the matrix after.
  * @param n The number of letters in the sequence to display*/
-static void displayUpdateLoop(int n)
+static void display_update_loop(int n)
 {
-    int loopFor = n * makeSameHz(PLAYBACK_RATE);
+    int loop_for = n * display_get_hz(PLAYBACK_RATE);
     int counter = 0;
-    while(counter <= loopFor) {
+    while(counter <= loop_for) {
         tinygl_update();
         pacer_wait();
         counter++;
     }
-    matrixClear();
+    display_matrix_clear();
 }
 
 /**Sets the text to be display on the LED matrix using tinygl.
  * This function is for displaying information text for the game, this
  * type of text is dislplayed TEXT_MULTIPLIER times and in the TINYGL_TEXT_MODE_SCROLL
  * text mode.
- * It then calls displayUpdateLoop with the argument TEXT_MULTIPLIER * n
+ * It then calls display_update_loop with the argument TEXT_MULTIPLIER * n
  * @param text The char* of text to be displayed on the LED matrix
  * @param n The length of the text to be displayed on the LED matrix
  *
@@ -64,14 +66,14 @@ void display_text(char* text, int n)
 {
     tinygl_text(text);
     tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
-    displayUpdateLoop(TEXT_MULTIPLIER * n);
+    display_update_loop(TEXT_MULTIPLIER * n);
 }
 
 
 /**
  * This function sets the tinygl_text to the sequence of movements to
  * be done by the player. This is displayed in the TINGL_TEXT_MODE_STEP
- * Finally it calls the displayUpdateLoop with the argument n
+ * Finally it calls the display_update_loop with the argument n
  *
  * @param sequence The char* of moves to be dones by the player
  * @param n The number of characters in the sequence*/
@@ -79,14 +81,29 @@ void display_sequence(char* sequence, int n)
 {
     tinygl_text_mode_set(TINYGL_TEXT_MODE_STEP);
     tinygl_text(sequence);
-    displayUpdateLoop(n);
+    display_update_loop(n);
+}
+
+/**
+ * Displays a given integer on the LED Matrix by converting using itoa()
+ * it to a char* that can be sent to display_sequence
+ * 
+ * @param int int_to_display, the integer to be displayed
+ * */
+void display_int(int int_to_display)
+{
+	int length = 2;
+	int base = 10;
+	char char_of_int[length];
+	itoa(int_to_display / 2, char_of_int, base);
+	display_sequence(char_of_int, length);
 }
 
 /**
  * Intialises other modules or variables needed for the
  * game_display module.
  **/
-void gameDisplay_init (void)
+void game_display_init (void)
 {
     tinygl_init (PACER_RATE);
     tinygl_font_set (&font5x7_1_arrows);
