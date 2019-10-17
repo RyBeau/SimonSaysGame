8ce5f9737d7f81414a1f9a8904542bd05b33a562
Youre Name <rbe72@uclive.ac.nz>
2019-10-17 19:55:06 +1300

    Changed variable names to be more readable and removed unused variable

--- navinput.c
+++ navinput.c
@@ -1,5 +1,5 @@
 /*
- * Implements navswitch and processes the player's input sequence.
+ * Implements navswitch and processes the player's sequence sequence.
  *
  * @author Raymond Tamse, Ryan Beaumont
  * Date: October 15 2019
@@ -11,43 +11,43 @@
 #include "game_display.h"
 
 /**
- * Takes a char* input that gets updated depending on the player's
- * input. Loops n times, where n/2 is the num. of symbols - the
+ * Takes a char* sequence that gets updated depending on the player's
+ * sequence. Loops current_size times, where current_size/2 is the num. of symbols - the
  * other half is for spaces.
- * @param char* input, player sequence to be updated with input
+ * @param char* sequence, player sequence to be updated with sequence
  */
-void player_input(char* input, int n, int is_turn)
+void player_input(char* sequence, int current_size)
 {
     int counter = 0;	
-    while (counter < n) {
+    while (counter < current_size) {
         navswitch_update();
         if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
-            input[counter] = '^';
-            input[counter + 1] = ' ';
+            sequence[counter] = '^';
+            sequence[counter + 1] = ' ';
             counter += 2;
         }
 
         if (navswitch_push_event_p (NAVSWITCH_EAST)) {
-            input[counter] = '>';
-            input[counter + 1] = ' ';
+            sequence[counter] = '>';
+            sequence[counter + 1] = ' ';
             counter += 2;
         }
 
         if (navswitch_push_event_p (NAVSWITCH_SOUTH)) {
-            input[counter] = '.';
-            input[counter + 1] = ' ';
+            sequence[counter] = '.';
+            sequence[counter + 1] = ' ';
             counter += 2;
         }
 
         if (navswitch_push_event_p (NAVSWITCH_WEST)) {
-            input[counter] = '<';
-            input[counter + 1] = ' ';
+            sequence[counter] = '<';
+            sequence[counter + 1] = ' ';
             counter += 2;
         }
 
         if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-            input[counter] = '*';
-            input[counter + 1] = ' ';
+            sequence[counter] = '*';
+            sequence[counter + 1] = ' ';
             counter += 2;   // dot
         }
         pacer_wait();
