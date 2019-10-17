6b67fc422ada0d7a6c53c3630bee5dd3fd67279a
Youre Name <rbe72@uclive.ac.nz>
2019-10-17 23:32:51 +1300

    Function names change to be more clear

--- communication.h
+++ communication.h
@@ -1,3 +1,10 @@
+/**
+ * @author Ryan Beaumont
+ * @author Raymond Tamse
+ * @date 11/10/2019
+ * @brief Header file for communication.c, definitions of function for
+ * interboard communication of sequences.
+ * */
 
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
@@ -8,14 +15,14 @@
  * This transmits the sequece given in char* sequence.
  * @param char* sequence, data to be sent to the other board
  * */
-void transmitSequence(char* sequence, int n);
+void comm_transmit_sequence(char* sequence, int n);
 
 /**
- * Receives the data sent by transmitSequence and puts it in the
+ * Receives the data sent by comm_transmit_sequence and puts it in the
  * given char* received.
  * @param char* received, char* for the received data to go into
  * */
-void receiveSequence(char* received, int n);
+void comm_receive_sequence(char* received, int n);
 
 /**
  * Intialises other modules or variables needed for the 
