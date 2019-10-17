284ec7a4a52ff4ce5224b2eabb9544228c90b3f7
Youre Name <rbe72@uclive.ac.nz>
2019-10-17 23:32:41 +1300

    Function names change to be more clear, comments added

--- communication.c
+++ communication.c
@@ -1,5 +1,6 @@
 /**
  * @author Ryan Beaumont
+ * @author Raymond Tamse
  * @date 11/10/2019
  * @brief Transmit to other Player
  * @note This module transmits game information to the other player.
@@ -10,8 +11,9 @@
 /**
  * This transmits the sequece given in char* sequence.
  * @param char* sequence, data to be sent to the other board
+ * @param int n, the length of the sequence to be transmitted
  * */
-void transmitSequence(char* sequence, int n)
+void comm_transmit_sequence(char* sequence, int n)
 {
     int counter = 0;
     while (counter < n || !ir_uart_write_finished_p ()) {
@@ -23,11 +25,11 @@ void transmitSequence(char* sequence, int n)
 }
 
 /**
- * Receives the data sent by transmitSequence and puts it in the
+ * Receives the data sent by comm_transmit_sequence and puts it in the
  * given char* received.
  * @param char* received, char* for the received data to go into
  * */
-void receiveSequence(char* received, int n)
+void comm_receive_sequence(char* received, int n)
 {
     int counter = 0;
     while (counter < n) {
