76110eed1e8d6a084e6f38d08c4ffd6bf69a0fba
Raymond Steve Jr Fernandez Tamse <rft22@cs17080jp.canterbury.ac.nz>
2019-10-17 20:36:50 +1300

    Updated docstrings

--- navinput.h
+++ navinput.h
@@ -12,8 +12,8 @@
 
 /**
  * Takes a char* sequence that gets updated depending on the player's
- * sequence. Loops n times, where n/2 is the num. of symbols - the
- * other half is for spaces.
+ * sequence. Loops current_size times, where current_size/2 is the num.
+ * of symbols - the other half is for spaces.
  * @param char* sequence, player sequence to be updated with sequence
  */
 void player_input(char* sequence, int current_size);
